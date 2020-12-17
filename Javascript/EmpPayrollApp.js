7aabf153de4a29d86f469fe4becc8b99117d595c
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-12-17 15:45:47 +0530

    [Prajakta]: Refactor. salary range functionality already added

--- Javascript/EmpPayrollApp.js
+++ Javascript/EmpPayrollApp.js
@@ -1,5 +1,7 @@
+//UC8 salary Range
 const salary = document.querySelector('#salary');
 const output = document.querySelector('.salary-output');
+//in range button the output shown should always be equal to value the user is updating
 output.textContent = salary.value;
 salary.addEventListener('input', function () {
     output.textContent = salary.value;
