15413c5df76cef232ab69fcc3732ba7688a527f0
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-12-18 08:07:01 +0530

    [Prajakta]: Add. new attributes with employee payroll class

--- Javascript/EmployeePayroll.js
+++ Javascript/EmployeePayroll.js
@@ -1,46 +1,85 @@
-class EmployeePayrollData 
-{
-    constructor(...params) {
-      this.id = params[0];
-      this.name = params[1];
-      this.salary = params[2];
-      this.gender = params[3];
-      this.date = params[4];
-    }
+class EmployeePayrollData  {
 
-    get name() {
-      return this._name;
-    }
-    set name(name) {
-      let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
-       
-        if (nameRegex.test(name)) 
-        this._name = name;
-        else throw "Name is incorrect";
-      } 
+//getter and setter method
+get id() { 
+    return this._id; 
+}
+
+set id(id) { 
+    this._id = id;
+}
+
+get name() {
+    return this._name;
+}
+
+set name(name) {
+    let nameRegex=RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(name))
+        this._name=name;
+    else throw 'Name is Incorrect!';
+}
+
+get profilePic() {
+    return this.profilePic;
+}
+set profilePic(profilePic) {
+    this.profilePic = profilePic;
+}
+
+get gender() {
+    return this._gender;
+}
+
+set gender(gender) {
+    this._gender = gender;
+}
+
+get department() {
+    return this._department;
+}
+
+set department(department) {
+    this._department = department;
+}
+
+get salary() {
+    return this._salary;
+}
+
+set salary(salary) {
+    this._salary = salary;
+}
+
+get note() {
+    return this._note;
+}
+
+set note(note) {
+    this._note = note;
+}
+
+get startDate() {
+    return this._startDate;
+}
+
+set startDate(startDate) {
+//if it is a future date it will throw error
+    if (startDate > new Date()) // date function will provide todays date
+    throw "Must not be Future Date";
+    else 
+    this._startDate = startDate;
+}
 
 
-    
-    get startDate() {
-        return this._startDate;
-    }
 
-    set startDate(startDate) {
-    //if it is a future date it will throw error
-        if (startDate > new Date()) // date func will provide todays date
-        throw "Must not be Future Date";
-        else 
-        this._startDate = startDate;
-    }
-
-      //method
-      toString() {
-        const options = { year: 'numeric', month: 'short', day: 'numeric' };
-        //if start date is not in the given datatype it will pass the value undefined to empdate
-        const empDate = !this.startDate ? "undefined" :
-        this.startDate;
-        //here in return for start date we are passing empDate as value                
-        return "id=" + this.id + ", name='" + this.name + ", gender='" + this.gender +
-            ", salary=" + this.salary + ", startDate=" + empDate;
-    }
+//method
+toString() {
+    const options = { year: 'numeric', month: 'short', day: 'numeric' };
+    const empDate = !this.startDate ? "undefined" : 
+                    this.startDate;
+    return "id=" + this.id + ", name='" + this.name + ", gender='" + this.gender + 
+           ", profilePic='" + this.profilePic + ", department=" + this.department +
+           ", salary=" + this.salary + ", startDate=" + empDate + ", note=" + this.note;
+  }
 }
