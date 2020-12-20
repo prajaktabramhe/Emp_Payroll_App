e95b851f9b9728bee90243fa3584d3b50f728dd0
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-12-20 23:59:14 +0530

    [Prajakta]: Add. Display employee details from JSON object

--- Javascript/NewPayrollHome.js
+++ Javascript/NewPayrollHome.js
@@ -1,23 +1,64 @@
+const createEmployeePayrollJSON = () => { 
+    let empPayrollListLocal = [
+        {
+            _name:'Prajakta Bramhe',
+            _gender:'Female',
+            _department:[
+                'Engineering','Hr'
+            ],
+            _salary:'500000',
+            _startDate:'20 oct 2020',
+            _note:'',
+            _id:new Date().getTime(),
+            _profilePic:'../Assets/profile_4.jpg'
+        },
+        {
+            _name:'Sanket Bramhe',
+            _gender:'Male',
+            _department:[
+                'Engineering','Sale'
+            ],
+            _salary:'400000',
+            _startDate:'25 oct 2020',
+            _note:'',
+            _id:new Date().getTime(),
+            _profilePic:'../Assets/profile_1.png'
+        }
+    ]
+    return empPayrollListLocal
+}
+
 window.addEventListener('DOMContentLoaded', (event) => {
     createInnerHtml();
-});
-const createInnerHtml = () => {
-    const headerHtml = "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th>"+
-        "<th>Salary</th><th>Start Date</th><th>Actions</th></tr>";
-    const innerHtml = `${headerHtml}
+})
 
-<tr>
-    <td><img class="profile" src="../Assets/profile_4.jpg" width="30px" alt="profile4"></td>
-    <td>Prajakta Bramhe</td>
-    <td>Female</td>
-    <td><div class="dept-label">HR</div>
-    <div class="dept-label">Finance</div></td>
-    <td>3000000</td>
-    <td>1 Nov 2020</td>
-    <td>
-    <img id="1" onclick="remove(this)" alt="delete" src="../Assets/icons/delete.png">
-    <img id="1" alt="create"onclick="update(this)" src="../Assets/icons/create.png">
-</td>
-</tr>`;
-document.querySelector('#table-display').innerHTML = innerHtml;
+function createInnerHtml() {
+    const headerHtml = `<tr><th></th><th>Name</th><th>Gender</th><th>Department</th>
+        <th>Salary</th><th>start Date</th><th>Actions</th></tr>`    
+    let innerHtml = `${headerHtml}`
+    let empPayrollList = createEmployeePayrollJSON()
+    for(let empPayrollData of empPayrollList) {
+        innerHtml = `${innerHtml}
+        <tr>
+        <td><img src="${empPayrollData._profilePic}" class="profile" width="30px" alt=""></td>
+        <td>${empPayrollData._name}</td>
+        <td>${empPayrollData._gender}</td>
+        <td>${getDeptHtml(empPayrollData._department)}</td>
+        <td>${empPayrollData._salary}</td>
+        <td>${empPayrollData._startDate}</td>
+        <td>
+            <img id="1" name="${empPayrollData._id}" onclick="remove(this)" alt="delete" width="30px" src="../Assets/icons/delete.png">
+            <img id="1" name="${empPayrollData._id}" onclick="update(this)" alt="edit" width="30px" src="../Assets/icons/create.png">
+        </td>
+    </tr>`
+    }
+    document.querySelector('#display').innerHTML = innerHtml
+}
+function getDeptHtml(deptList) 
+{
+    let deptHtml = ''
+    for(const dept of deptList) {
+        deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
+    }
+    return deptHtml
 }
